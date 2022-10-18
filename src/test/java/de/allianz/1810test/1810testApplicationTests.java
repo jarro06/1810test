f716b8290f9a88754f629e0702a0334f41857203
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-10-18 12:39:29 +0200

    Create maven project

---
+++ src/test/java/de/allianz/1810test/1810testApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.1810test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class 1810testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
